c2ed96682077a249cf168ee3b097b6440f70ac9e
satvikaryan <satvikaryan200429@gmail.com>
2025-02-22 14:06:52 +0530

    landing page

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+import random
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
+client = MongoClient("mongodb://localhost:27017/")
+db = client["hci"]
+product_collection = db["Product"]
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    products = list(product_collection.find())
+    random_products = random.sample(products, min(len(products), 3))
+    # print("Random Products:", random_products)
+    return render_template("index.html", random_products=random_products)
 
 # ...
 @app.route('/about/')
@@ -13,3 +21,4 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
